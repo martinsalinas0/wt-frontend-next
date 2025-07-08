29437eefb34b0aeb80a40667a1bc18509795a16e
Martin <martinlsalinas@pm.me>
2025-07-07 19:43:26 -0500

    Add Jobs and User pages, enhance NavBar with navigation links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <div className="bg-amber-500 text-3xl">NavBar</div>;
+  return (
+    <div className="bg-stone-900 text-3xl">
+      <nav className="min-w-auto bg-darkblue shadow-2xl px-6 py-4">
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-6">
+            <p className="text-orange-400 hover:text-orange-800 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/">HOME</Link>
+            </p>
+            <p className="text-orange-400 hover:text-orange-800 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/jobs">Jobs</Link>
+            </p>
+            <p className="text-orange-400 hover:text-orange-800 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/settings">Settings</Link>
+            </p>
+          </div>
+          <div className="text-orange-500 font-bold px-3 py-2">
+            <Link href="/user">User</Link>{" "}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
