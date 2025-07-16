3d98a022c1ff25f681fb3d3d2a30d5293f9e4742
martinsalinas0 <martinlsalinas@pm.me>
2025-07-15 22:33:11 -0500

    Add User Settings page and enhance User Page with navigation link

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const UserPage = () => {
   return (
     <div className="flex flex-col items-center px-4 mt-10">
       <h1 className="font-bold text-6xl mb-6">User Page</h1>
+      <p className="font-bold uppercase 4xl text-blue-500">
+        <Link href="/user/settings">CLICK</Link>
+      </p>
       <div className="text-center max-w-xl text-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
         praesentium non quod recusandae atque fugiat necessitatibus adipisci
---
+++ src/app/user/settings/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserSetting = () => {
+  return <div>UserSettings</div>;
+};
+
+export default UserSetting;
