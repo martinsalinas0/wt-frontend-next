2d1793b7178590998683b97ad9ac32ce7957b7ce
martinsalinas0 <martinlsalinas@pm.me>
2025-07-17 17:57:51 -0500

    Add UserCard component for displaying user information

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserCard = () => {
+  return (
+    <div className="border-2 border-stone-700">
+      UserCard
+      <h1>USER NAME John Doe</h1>
+      <span>
+        email phone number user id password **** address alt number role jobs
+        completed jobs posted rating reviews
+      </span>
+    </div>
+  );
+};
+
+export default UserCard;
