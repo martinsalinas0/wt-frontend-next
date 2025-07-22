bf54aa36b758b9f8c2b1e4df8961a30642a6cbae
martinsalinas0 <martinlsalinas@pm.me>
2025-07-21 21:38:47 -0500

    feat: implement JobsTestPage with API integration to fetch job listings

--- src/app/jobs-test/page.tsx
+++ src/app/jobs-test/page.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const JobsTestPage = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const getJobs = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/jobs/all`);
+
+        setJobs(response.data.jobs);
+      } catch (error) {
+        console.error("Failed to GET from API: ", error);
+      }
+    };
+    getJobs();
+  }, []);
+
+  console.log(jobs);
+
   return <div>JobsTestPage</div>;
 };
 
